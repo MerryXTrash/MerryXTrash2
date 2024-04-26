3c9685f39585fe5bf664d777044f8f39f783d3b4
MerryXTrash <158755815+MerryXTrash@users.noreply.github.com>
2024-04-26 20:14:35 +0700

    Update Aimbot.lua

--- Aimbot.lua
+++ Aimbot.lua
@@ -4,7 +4,6 @@ end
 
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
 local Window = Library.CreateLib("Aimbot | ไว้ยิงไก่แอ๋ม", "BloodTheme")
-
 local Tab = Window:NewTab("แอ๋มกากเล่นโปร")
 Section:UpdateSection("ล็อคหัวไก่แอ๋ม")
 Section:NewButton("กดเพื่อยิงไก่", "UwuUUUu", function()
