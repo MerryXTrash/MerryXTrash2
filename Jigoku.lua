9afaf2a2f85ad95e3fe0693c96c87e7d613a009c
x3Catsusz <158755815+MerryXTrash@users.noreply.github.com>
2024-08-18 22:38:18 +0700

    Update Jigoku.lua

--- Jigoku.lua
+++ Jigoku.lua
@@ -90,7 +90,7 @@ end
 
 -- Settings for teleportation
 local teleportOffset = Vector3.new(0, 5, 0)  -- Default offset above the orb
-local waitTime = 1  -- Default wait time between teleportations
+local waitTime = 0.1  -- Default wait time between teleportations
 
 -- Function to teleport player to the highest Orb (above its head) and fire ProximityPrompt
 local function teleportToHighestOrbAboveHead()
